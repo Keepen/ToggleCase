1fdaf1aad04afde3498a4ccd64a9b37652e5202c
Keepen <46278766+Keepen@users.noreply.github.com>
2019-04-19 22:55:12 +0800

    大小写转换
    大小写转换

---
+++ 大小写转换输出（优化）/大小写转换输出（优化）/大小写转换输出（优化）.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+	char a='c';
+	printf("%d", a);
+	printf("%c\n\n\n", a);
+	char c = 97;
+	printf("%d", c);
+	printf("%c\n\n\n", c);
+	int b = 'a';
+	printf("%d", b);
+	printf("%c\n\n\n", b);
+	int d = 110;
+	printf("%c", d);
+	system("pause");
+}
